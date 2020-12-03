31c6e1494b890dc6b6de3129bba4cc66ee13a56a
Oscar Bennet <32869912+obennet@users.noreply.github.com>
2020-12-03 11:49:44 +0100

    day1 improved

--- src/day1/index.js
+++ src/day1/index.js
@@ -7,25 +7,18 @@ var product3 = 0;
 
 for(var i = 0; i<textByLine.length; i++){
     for(var j = 1; j<textByLine.length; j++){
+        for(var x = 2; x<textByLine.length; x++){
+            if(textByLine[i] + textByLine[j] + textByLine[x]== 2020){
+                product3 = textByLine[i]*textByLine[j]*textByLine[x];
+                break;
+                }
+        }
         if(textByLine[i] + textByLine[j] == 2020){
             product2 = textByLine[i]*textByLine[j];
             break;
         }
     }
 }
-
-for(var i = 0; i<textByLine.length; i++){
-    for(var j = 1; j<textByLine.length; j++){
-        for(var x = 2; x<textByLine.length; x++){
-            if(textByLine[i] + textByLine[j] + textByLine[x]== 2020){
-                product3 = textByLine[i]*textByLine[j]*textByLine[x];
-                break;
-                }
-        
-        }
-    }
-}
-
 
 console.log("Part 1: " + product2);
 console.log("Part 2: " + product3);
